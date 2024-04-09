c86e89605808525167a9f39dd4d039a4fc0b1c83
eulptr <e.skelaite@gmail.com>
2024-04-09 15:26:07 +0300

    0409almost works su plytom

--- src/test/java/testas.java
+++ src/test/java/testas.java
@@ -14,6 +14,7 @@ public class testas {
     @BeforeTest
     public void setUp() {
         _globalDriver = new ChromeDriver();
+        _globalDriver.manage().window().maximize();
     }
 
     @Test
@@ -47,6 +48,38 @@ public class testas {
 
         System.out.println("Tikru skelbimu yra " + count);
         }
-    }
+
+        @Test
+    public void samotinesPlytos(){
+            int count = 0;
+
+
+            for (int p = 1; p <= 5; p++) {
+                _globalDriver.get("https://www.skelbiu.lt/skelbimai/" + p + "?keywords=samotines+plytos");
+
+                if (p==1) {
+                    _globalDriver.findElement(By.xpath("/html/body/div[10]/div[2]/div/div[1]/div/div[2]/div/button[2]")).click();
+                }
+                for (int i = 1; i <= 27; i++) {
+                    //html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[2]/div[i]/a
+                    //html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[2]/div[2]/a
+                    //html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[3]/div[1]/a
+                    //html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[3]/div[2]/a
+                    try {
+                        _globalDriver.findElement(By.xpath("html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[" + (p+1) + "]/div[" + i + "]/a")).click();
+                        count++;
+                    } catch (Exception e) {
+                        continue;
+                    }
+                    String ID = _globalDriver.findElement(By.xpath("//*[@id=\"contentArea\"]/div[6]/div[1]/div[1]/div[4]/div[1]")).getText();
+                    System.out.println(ID);
+                    _globalDriver.get("https://www.skelbiu.lt/skelbimai/" + p + "?keywords=samotines+plytos");
+
+                }
+            }
+
+            System.out.println("Tikru skelbimu yra " + count);
+        }
+        }
 
 
